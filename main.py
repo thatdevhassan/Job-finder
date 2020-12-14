faf5d994619ae024865ac3bb1deaab7d05c36e75
thatdevhassan <thatdevhassan@gmail.com>
2020-12-14 14:40:59 -0500

    feat: getting temperature and short description

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-import requests
+# scraping data from a wether forcast website.
+# date: 14 December
+
 import bs4
+import requests
 from bs4 import BeautifulSoup
 
 
-# get the link
-# ask the user for preferences
-# scape data using bs4
-#currently targeting  linkedin
+with open('index.html','r') as parsefile:
+    soup = BeautifulSoup(parsefile, 'html.parser')
+
+# main    
+forecast_list = soup.find('ul', id="seven-day-forecast-list")
+
+for insidelist in forecast_list.find_all('li' ,class_='forecast-tombstone'):
+    # print(insidelist.text)
+    desc = insidelist.find('p',class_='short-desc')
+    temp = insidelist.find('p', class_='temp')
+    print("description " ,desc.get_text().strip())
+    print("temperatire " ,temp.get_text().strip())
+    print("\n")
+    
+
+
